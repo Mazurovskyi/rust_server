7877d895393378206c6b69c2321692b34a7ed374
vuser <mazurovskiyyevgen@gmail.com>
2023-06-27 11:04:10 +0300

    empty project

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::{
+    io::{prelude::*, BufReader, self},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for (i, stream) in listener.incoming().enumerate(){
+        let stream = stream.unwrap();
+        println!("stream detected: {stream:?}");
+        handle(stream);
+    }
+}
+
+fn handle(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&mut stream);
+
+    let request: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line|!line.is_empty())
+        .collect();
+
+    
+    //println!("request: {request:?}");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
