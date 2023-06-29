aa1808bf73c6c8d690387f44b3a229f77fb9442c
vuser <mazurovskiyyevgen@gmail.com>
2023-06-29 17:21:18 +0300

    Sara add more print

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ fn handle(mut stream: TcpStream){
     
     println!("request: {request:?}");
     println!("INSERTION SARA");
+    println!("INSERTION SARA 2");
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
     stream.write_all(response.as_bytes()).unwrap();
